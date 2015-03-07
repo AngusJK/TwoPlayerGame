b9245d6e929cc04c98db1a2dc20e53fd9faba7f0
AngusJK <anguskellett@hotmail.com>
2015-03-07 02:11:37 +0000

    corrects scope

--- player.rb
+++ player.rb
@@ -1,49 +1,55 @@
 require 'pry'
-class Player
-  def initialize(name)
-    @lives = 3
-    @name = name
-  end
+require 'pry-nav'
 
-  def random_number_generator
-    @num1 = rand(1..20)
-    @num2 = rand(1..20)
-    @answer = @num1 + @num2
+class Player
+  attr_accessor :lives, :name 
+
+  def initialize(name)
+    @name = name
+    @lives = 3
   end
 
   def question_generator
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @answer = @num1 + @num2
     puts "#{@name}, what is #{@num1} plus #{@num2}?"
-  end 
+    @player_answer = gets.chomp.to_i
+  end
 
   def verify
-    unless @player_answer == @answer
-      @lives -= 1
-      puts "You lose!" if @lives == 0
-      puts @lives
+    if @player_answer == @answer
+      puts "Correct!"
+    else @lives -= 1
+        if @lives == 0
+          puts "You lose!"
+        else  
+          puts "#{@name}'s lives is now at: #{@lives}"
+        end
     end
   end
 end
 
-puts "Player 1, what is your name?"
-player1 = Player.new(gets.chomp)
+#binding.pry
 
-puts "Player 2, what is your name?"
-player2 = Player.new(gets.chomp)
+def game
+  puts "Player 1, what is your name?"
+  player1 = Player.new(gets.chomp)
 
-binding.pry
+  puts "Player 2, what is your name?"
+  player2 = Player.new(gets.chomp)
+  while player1.lives > 0 && player2.lives > 0
+    player1.question_generator
 
-while player1(lives) > 0 && player2(lives) >0
-player1.question_generator
-@player_answer = gets.chomp.to_i
+    player1.verify
 
-player1.verify
+    player2.question_generator
 
-player2.question_generator
-player_answer = gets.chomp.to_i
-
-player2.verify
+    player2.verify
+  end
 end
 
+game
 
 
 
